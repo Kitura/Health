a26e0794213862ba918a21d7811bb449ad0e66fd
rolivieri <roliv@us.ibm.com>
2017-06-12 15:43:24 -0500

    Adding jazzy docs

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-/**
+/*
 * Copyright IBM Corporation 2017
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +12,13 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-**/
+*/
 
 import PackageDescription
 
 let package = Package(
   name: "Health",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1),
+    .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1)
   ]
 )
